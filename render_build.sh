8fc84d0d5c0eff490e4a81cd31a17e6bb2bbf866
jollylava <jollylava33@gmail.com>
2025-10-18 17:36:56 +0200

    all

---
+++ render_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit  # exit on error
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py migrate
+python manage.py collectstatic --noinput
